44ff794cca8713626c1278401112f4ba381175fc
arstan6ekovich <zomadujsonov@gmail.com>
2025-08-15 06:32:18 +0600

    mar4ik

--- next.config.js
+++ next.config.js
@@ -1,21 +1,31 @@
 /** @type {import("next").NextConfig} */
 const nextConfig = {
-	reactStrictMode: true,
+  reactStrictMode: true,
 
-	i18n: {
-		locales: ['en', 'ru'],
-		defaultLocale: 'en',
-		localeDetection: false
-	},
+  i18n: {
+    locales: ['en', 'ru'],
+    defaultLocale: 'en',
+    localeDetection: false
+  },
 
-	webpack(config) {
-		config.module.rules.push({
-			test: /\.svg$/,
-			use: [{ loader: '@svgr/webpack', options: { icon: true } }]
-		});
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: [{ loader: '@svgr/webpack', options: { icon: true } }]
+    });
 
-		return config;
-	}
+    return config;
+  },
+
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        destination: 'https://mar4ik.pro/:path*',
+        permanent: true,
+      },
+    ]
+  },
 };
 
 module.exports = nextConfig;
